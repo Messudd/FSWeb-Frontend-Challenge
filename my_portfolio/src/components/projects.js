44c50e1abccf1f621672caab84d02b814a51a1bf
Messudd <90100309+Messudd@users.noreply.github.com>
2023-09-15 12:48:05 +0300

    slider_update

--- my_portfolio/src/components/projects.js
+++ my_portfolio/src/components/projects.js
@@ -15,7 +15,7 @@ const Projects = () => {
     infinite: false,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
     initialSlide: 0,
     nextArrow: <Arrow />,
     prevArrow: <Arrow />,
